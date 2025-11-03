d229083e2688c2048c347a5873897f3233f1ae92
Aleksandr Chernov <sashacool75@gmail.com>
2025-11-03 23:59:51 +0300

    delete doc

--- lib/helpers/format.ex
+++ lib/helpers/format.ex
@@ -1,15 +1,4 @@
 defmodule Helpers.Format do
-  @moduledoc """
-  Module for format strings
-  """
-
-  @doc """
-  Trim text
-
-  ## Examples:
-
-  "Hello world   " => "Hello world"
-  """
   def trim_text(string) do
     String.trim(string)
   end
