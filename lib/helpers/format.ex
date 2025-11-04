b551d908671628e22887a1f67812414b4b7179ea
Aleksandr Chernov <sashacool75@gmail.com>
2025-11-04 03:24:07 +0300

    research pipe operator

--- lib/helpers/format.ex
+++ lib/helpers/format.ex
@@ -2,4 +2,11 @@ defmodule Helpers.Format do
   def trim_text(string) do
     String.trim(string)
   end
+
+  def generate_slug(text) do
+    text
+    |> String.trim()
+    |> String.downcase()
+    |> String.replace(" ", "-")
+  end
 end
