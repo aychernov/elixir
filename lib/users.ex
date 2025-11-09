995f85eaa0b63458c9fc6f18a406f1189ae140cc
Aleksandr Chernov <sashacool75@gmail.com>
2025-11-09 18:14:31 +0300

    Struct

--- lib/users.ex
+++ lib/users.ex
@@ -1,22 +1,26 @@
 defmodule Users do
+  @enforce_keys [:name]
+  defstruct name: "user", age: nil, is_cool: nil, can_drink: nil
   alias Helpers.Format
 
+  def check(%Users{}), do: true
+  def check(_users), do: false
   # new/2
   def new("Sasha", age) do
-    %{name: "Sasha", age: age, is_cool: true, can_drink: true}
+    %Users{name: "Sasha", age: age, is_cool: true, can_drink: true}
   end
 
   # new/2
   def new(name, age) when is_number(age) and age >= 18 do
     formatted_name = Format.trim_text(name)
-    %{name: formatted_name, age: age, is_cool: false, can_drink: true}
+    %Users{name: formatted_name, age: age, is_cool: false, can_drink: true}
     dbg()
   end
 
   # new/2
   def new(name, age) when is_number(age) do
     formatted_name = Format.trim_text(name)
-    %{name: formatted_name, age: age, is_cool: false, can_drink: false}
+    %Users{name: formatted_name, age: age, is_cool: false, can_drink: false}
   end
 
   def new(_, _) do
@@ -27,6 +31,6 @@ defmodule Users do
 
   # new/0  def new, do: generate_default_user()
   defp generate_default_user do
-    %{name: "Doe", age: 24, is_cool: false, can_drink: false}
+    %Users{name: "Doe", age: 24, is_cool: false, can_drink: false}
   end
 end
